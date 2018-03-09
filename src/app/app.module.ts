d27354ff4060f445184015369ad9501a258bfabe
R2D2\Ivan <vanuy1305@gmal.com>
2018-03-09 21:34:25 +0200

    push all stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {NgxPageScrollModule} from 'ngx-page-scroll';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
