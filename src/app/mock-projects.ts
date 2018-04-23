796aa846d4f65190a7e744042c50521e0ea29484
whike <vanuy1305@gmail.com>
2018-04-23 15:51:19 +0300

    add new projects

--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -4,117 +4,99 @@ export const PROJECTS: Project[] = [
       {
         id: 1,
         name: 'Jupiter towers',
-        address: 'Nesbit Rd, Tadwadi, Mazgaon, Mumbai, Maharashtra',
+        address: 'Mazgaon, Mumbai',
         new_project: false,
         img_path: 'test.jpg',
         description:
         `
-        Jupiter tower has got breathtaking view of Arabic ocean and Mazgaon Garden view
-        Thus satisfying the need for sense of vastness and green, giving huge pride for the owners,
-        its one of the first few tallest towers in Mazgaon and
-        the first building to integrate glass façade into the contemporary elevation style.
-        One of the other highlighting features of the tower is 100% usable carpet area devoid of flower bed and dry balcony,
-        providing the buyer with very high value for the investment so done.
-        The G + 23 building is designed by kutub Mandviwala, one of the top notch architect of Mumbai.
-        The 2bhk and 3 bhk apartments are distributed as 3 flats in each floor.
-        The 2 lacs square feet of development on plot size of 3500sq mtrs, has provided an ample free space in and around
-        the building for open parking of 30 cars and basement parking of 50 cars, with refugee on 7th, 14th and 21st floor`
+        A breathtaking view of the Arabian Sea and the green canopy of Mazgaon
+        Garden. It gives a huge pride to know that it is one of the tallest towers
+        in Mazgaon and the first building to integrate glass façade into the
+        contemporary elevation style.
+        Well connected and easily accessible, this project has added a new
+        dimension to the Mazgaon’s heritage in South Mumbai
+        `
       },
       {
         id: 2,
-        name: 'Ketan apartments, Mazgaon',
-        address: 'Bhagwan Adinath Marg, Ekta Nagar, Mazgaon, Mumbai, Maharashtra',
+        name: 'Ketan apartment',
+        address: 'Mazgaon, Mumbai',
         new_project: false,
         img_path: 'test.jpg',
         description:
         `
-        Located on the Eastern Expressway and Lal Baugh Flyover,
-        Mazgaon is a high-density suburb in Mumbai South.
-        It offers ready to move in flats that meet the pocket needs of all income groups.
-        The realty zone of Mumbai is well organized as the city offers a great connectivity with road network.
-        It’s a standalone residential building of G+ 8 structures. It constitutes of four 2 Bhk flats on each floor.
-        The building is equipped with high speed elevator.
-        The residence of this building has got wonderful view of Mazgaon Park,
-        thus making their life green and serene amongst the concrete skylines.
-        Each residence flat is further fine tuned with high end vitrified tiles and bathroom accessories.
-        The building is very well connected, at a walkable distance to the local social elements,
-        market and transportation networks, thus fulfilling the everyday needs in a comfortable way.
+        Ketan Apartments brings luxury living to Mazgaon. Offering refined living
+        spaces amidst wide open areas and within city precincts.
+        e alluring architectural marvel of Ketan apartments ensures
+        unobstructed view of Mazgaon Garden and great connectivity is offered
+        with all networks of transportation in the city.
         `
       },
       {
         id: 3,
-        name: 'Nagsen apartment Mazgoan',
-        address: 'Nagsen Chs, Mazgaon, Mumbai Harbour, Mumbai',
+        name: 'Nagsen tower',
+        address: 'Mazgaon, Mumbai Harbour',
         new_project: false,
         img_path: 'test.jpg',
         description:
         `
-        It’s a standalone residential building with seven floors. It has got two wings A & B.
-        The building comprising of two wings ‘A & B’ constitutes of 1Bhk of 350 sq feet built up.
-        The totals of 174 flats are further served with 4 high speed elevators.
-        The building is attached with play area for kids.
-        Each residence unit is further finely fit with high quality tiles in the living and bedroom,
-        bathroom accessories and kitchen accessories.
-        The building is connected with local social requirements like school,
-        hospital and transportation networks at a walking distance of 3 mins.
+        The flats provide the luxury of convenience and space. e spacious
+        residence boasts of premium brands, quality materials and finishes so
+        luxurious, they will exceed your expectations.
+        Natural ventilation and ample sunlight will provide the right setting to
+        host your guests.
         `
       },
       {
         id: 4,
-        name: 'Sambhav darshan sandhurst raod',
-        address: 'Sandhurst Road (W), Navroji Hill Rd 7, Dongri, Umerkhadi, Mumbai, Maharashtra',
+        name: 'Sambhav darshan',
+        address: 'Sandhurst Road, Mumbai',
         new_project: false,
         img_path: 'test.jpg',
         description:
         `
-        Sambhav Darshan is G+7 building is combination of high street retails on the ground floor and residences from first floor onwards.
-        The 12 units of retails measure 200 sq feet of carpet, while the 58 units of residences are 1 Bhk types.
-        Sambhav Darshan is the first redeveloped building in the entire Sandhurst road.
-        It is also the first building to have elevator in the area of close vicinity.
-        The integrated complex is well connected to nearby social institutions like school,
-         hospital, theatre, mall and market and transport network.
-         It is 3 mins from Sandhurst road and 15 mins from classical Mumbai - CST, Fort and Churchgate etc.
-        Jain temple within the building premises makes the entire building, the preferred bargain for natives from Jain community.
+        It is an integrated combination of high street retail and luxury residences.
+        Its one of the iconic project of its time, have become a landmark for the
+        neighborhood.
+        Mixing the contemporary architecture into conventional designs which
+        meets the need of the customers. You can enjoy a sublime view of the sea,
+        a landscaped garden, and excellent connectivity to Mumbai’s prime
+        destinations.
         `
       },
       {
         id: 5,
         name: 'Sambhav tiara',
-        address: 'Plot no.16, L.T.Road no.1, M.G.Road, Mumbai, Maharashtra',
+        address: 'Goregaon, Mumbai',
         new_project: false,
         img_path: 'test.jpg',
         description:
         `
-        Sambhav Groups has launched its premium housing project, Tiara,
-         in Goregaon West, Mumbai. The project offers 1, 3.5 BHK Apartment
-         from 770 sqft to 1550 sqft in goregaon . Among the many luxurious
-         amenities that the project hosts are Car Parking,
-         Arty Lobby With Water Body Feature, Facility Management,
-         CCTV Camera, Lift, Rai­­­­­­­­­­­n Water Harvesting etc. Sambhav Groups Tiara is Completed project and possession in Dec 16.
+        Sambhav Tiara, the name roots from the crowned parametric elevation that
+        reflects the architectural achievement of the building.
+        e contemporary apartment building, makes it one of its kind with the
+        spectacular view and modern amenities.
         `
       },
       {
         id: 6,
-        name: 'Sambhav bungalows Lonavala',
-        address: 'Mirage Bungalow, Near, Lonavala',
+        name: 'Sambhav bungalows',
+        address: 'Mirage Bungalow, Lonavala',
         new_project: false,
         img_path: 'test.jpg',
         description:
         `
-        These are one of the first few bungalows to have mushroomed,
-        in the given vicinity that overlooks the deep valley of Lonavala mountains.
-        It’s a chain of 22 bungalows spread over a 1.5 acres of land.
-        The G+ 3 unit has got about 14 feet height in each floor with four bedroom & kitchen,
-        along with a balcony in each bedroom and a carpet area of 2000 sq feet.
-        The beautiful elevation was talk of the town, when the bungalows came into existence.
-        Each bungalow has got landscaped private garden, swing, party lawn and Kiosk with seating arrangements
-        facilitating a pleasurable family moments.
+        Shatrunjay Bungalows is a chain of 22 bungalows, spread over a land of
+        1.5 acres in Lonavala and is one of the first few projects which overlooks
+        the deep valley of Lonavala mountains.
+        A beautiful landscaped private garden is provided to each bungalow along
+        with party lawn, swing and kiosk with seating arrangements.
         `
       },
       {
         id: 7,
         name: 'Sambhav zest',
-        address: 'Bijjor CHSL, Waterfield Road, Bandra West, Mumbai, Maharashtra',
+        address: 'Bandra, Mombai',
         new_project: false,
         img_path: 'test.jpg',
         description:
@@ -129,28 +111,22 @@ export const PROJECTS: Project[] = [
       {
         id: 8,
         name: 'Siya Homes',
-        address: null,
+        address: 'Lonavala',
         new_project: false,
         img_path: 'test.jpg',
         description:
         `
-        It’s a scheme of six bungalows spread over a acre of land , occupied by cosmopolitans,
-        with more than 70% of the land is open accompanied with lush green garden area.
-        The bungalows are constructed on mountain slope with a direct and clear view of deep beautiful valley and fountains.
-        Siya homes give each stake holder an unrestricted access to deep valley,
-        thus fulfilling the curiosity of nature lovers, who seek adventure and fun.
-        The carpet area of each bungalow is approx 1500 sq feet, with a height of about 14 feet,
-        on both the ground and first floor, thus adding to a sense of spaciousness in the vastness of the nature.
-        Each bungalow is 3 Bhk unit with each bedroom balcony facing the valley and fountain.
-        Each bungalow has got private garden attached, catering to the private serenity of the owner.
-        The bungalow is maintained by caretakers, thus preserving the essence of the valuable property.
+        The Siya Homes project consists of six bungalow where 70% of the land is
+        lush green garden area. As the bungalows are constructed on mountain
+        slope, the balcony of each bungalow provides a clear view of beautiful
+        fountain and deep valley.
         `
       },
       {
         id: 9,
-        name: 'Darshan towers',
-        address: null,
-        new_project: true,
+        name: 'Darshan tower',
+        address: 'Mazgaon, Mumbai',
+        new_project: false,
         img_path: 'test.jpg',
         description:
         `
@@ -163,6 +139,119 @@ export const PROJECTS: Project[] = [
         The tower is well secured with high boundary wall, security equipments like CCTV, intercom,
         reception counter and security personnel.
         `
-      }
+      },
+      {
+        id: 10,
+        name: 'Premire Gold',
+        address: 'Sandhurst road, Mumbai',
+        new_project: false,
+        img_path: 'test.jpg',
+        description:
+        `
+        Previously known as Derby Talkies it is the first redeveloped move theatre
+        in Mumbai. e family entertaining theatre is one of its kind with old
+        charm and modern look. e luxury theatre is equipped with online booking
+        and dolby sound system.
+        `
+      },
+      {
+        id: 11,
+        name: 'Sambhav Vama',
+        address: 'Badlapur, Mumbai',
+        new_project: false,
+        img_path: 'test.jpg',
+        description:
+        `
+        Sambhav Vama is a premium housing project, launched by Sambhav Group
+        at the posh ane locality of Badlapur East. It has easy access to all the
+        modern amenities.
+        e project consists of luxury 1 BHK apartments with glass balcony. It’s
+        10min walking from the railway station.
+        `
+      },
+      {
+        id: 12,
+        name: 'The Primordial house',
+        address: 'Kemps Corner, Mumbai',
+        new_project: true,
+        img_path: 'test.jpg',
+        description:
+        `
+        Standing tall at the bustling hub of Kemp’s Corner, Primordial is the
+        quintessence of the French architectural marvels that reformulates the
+        meaning of luxury on a whole new level.
+        An enviable lifestyle and a glam statement in one, Primordial is your
+        destination abode that overlooks the Arabian Sea and the Hanging
+        Gardens. Well equipped with every facility and amenities at every corner,
+        the immaculate nature of this project have set a benchmark for most
+        future real estate projects.
+        `
+      },
+      {
+        id: 13,
+        name: 'Blue planet',
+        address: 'Mazgaon, Mumbai',
+        new_project: true,
+        img_path: 'test.jpg',
+        description:
+        `
+        It is the perfect residence in the south Mumbai, giving you the feeling that
+        everything was built around you. Located at Mazgaon, every day is a
+        celebration, because of the scenic beauty of Arabian sea and harbour view
+        on the other side which reflects contemporary Mumbai integrated into its
+        classic spine.
+        The unobstructed clear view upto 3kms radius up in the sky, makes you feel
+        like royalty with feet on the ground in sky.
+        `
+      },
+      {
+        id: 14,
+        name: 'Bay vue',
+        address: 'Marine lines, Mumbai',
+        new_project: true,
+        img_path: 'test.jpg',
+        description:
+        `
+        It has the most beautiful view of the Queens Necklace, presiding over
+        Marine Lines, South Mumbai. e elevation of the building is designed by
+        one of the best architect of the town ‘Sanjay Puri Associates’.
+        It redefines luxury living in India with an unparalleled experience for its
+        elite residents. Making the owners connect to the Nature and enjoy on day
+        to day basis the scenic beauty of the coastline.
+        `
+      },
+      {
+        id: 15,
+        name: 'Jai Bhavani',
+        address: 'Vile Parle, Mumbai',
+        new_project: true,
+        img_path: 'test.jpg',
+        description:
+        `
+        Rising majestically, Sambhav Zest is truly the pinnacle of luxury living.
+        Sambhav Zest's rich tapestry of experiences and a world-class lifestyle at
+        your disposal, everything here reminds you to live a life that is beyond
+        ordinary.
+        e project gives you the privilege of super spacious, luxurious residence
+        as well as a hobby room to give yourself a chance to explore passion
+        hobby whenever you feel like.
+        `
+      },
+      {
+        id: 16,
+        name: 'Chaitya 777',
+        address: 'Lalbaug, Mumbai',
+        new_project: true,
+        img_path: 'test.jpg',
+        description:
+        `
+        One of the premium projects in South Mumbai, it is a 43 floors high-rise
+        palatial residential tower based in Lalbaug enjoying the grand views of
+        Mahalakshmi Race course and Arabian Sea. is centrally located iconic
+        tower is aimed to provide luxurious and elegant houses with world class
+        amenities.
+        `
+      },
+
     ];
 
